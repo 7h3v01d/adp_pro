7b051b6396e20ffa52f1b2eb840fccf1babf7abd
Leon Priest <255045686+7h3v01d@users.noreply.github.com>
2026-07-26 01:14:41 +1000

    Delete packit.bat

--- packit.bat
+++
@@ -1,8 +0,0 @@
-python project_packager.py package . --exclude ".old/" --exclude "project_packager.py" --exclude "packit.bat" --exclude "pip.bat" --exclude "run.bat" --exclude "shell.bat" --exclude "doctor.bat" --exclude "sync.bat" --exclude "test.bat" --include ".gitignore" --exclude "*.zip" --exclude "*.rar" --exclude "PACKAGE_MANIFEST.json"
-pause
-
-
-
-
-
-
